21a9bc61653f6cf93c960863dc9bec58f71e79bd
mpearce25 <pearcematthew25@gmail.com>
2018-08-26 02:27:40 -0400

    updating cname

--- CNAME.py
+++ CNAME.py
@@ -8,6 +8,7 @@ end = filetext.index('\"', start, start + 50) -1
 
 heading1 = "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta http-equiv=\'Refresh\' content=\"4; URL=\'https://"
 url = filetext[start:end]
+print(url)
 nocaching = "<meta http-equiv=\'cache-control\' content=\'no-cache\'>"
 endtags = "\n\t</head>\n\t<body>\n\t\t<p>hello world</p>\n\t</body>\n</html>"
 
