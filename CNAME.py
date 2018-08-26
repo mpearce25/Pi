d8bd38a4a4d0d80e6e7097fefe67b45653d9e5ae
mpearce25 <pearcematthew25@gmail.com>
2018-08-26 01:18:56 -0400

    updating cname

--- CNAME.py
+++ CNAME.py
@@ -3,7 +3,8 @@ import sys
 file = open('web.txt', 'r+')
 filetext = file.read()
 start = filetext.index("ngrok.io", 0, len(filetext)) - 9
-url = filetext[start:start + 17]
+end = filetext.index('\"', start, start + 50)
+url = filetext[start:end]
 
 html = open('index.html','r+')
 html.write('%s%s%s' % ('<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta http-equiv=\'Refresh\' content=\"0;URL=\'https://',url ,'\'\"\\>\n\t</head>\n\t<body>\n\t\thello world\n\t</body>\n</html>'))
