e89240d8bb5d7f79e3751eb5807a0ada24fa136b
mpearce25 <pearcematthew25@gmail.com>
2018-08-26 02:18:02 -0400

    updating cname

--- CNAME.py
+++ CNAME.py
@@ -9,7 +9,7 @@ end = filetext.index('\"', start, start + 50) -1
 heading1 = "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta http-equiv=\'Refresh\' content=\"0; URL=\'https://"
 url = filetext[start:end]
 nocaching = "<meta http-equiv=\'cache-control\' content=\'no-cache\'>"
-endtags = "\n\t</head>\n\t<body>\n\t\thello world\n\t</body>\n</html>"
+endtags = "\n\t</head>\n\t<body>\n\t\t<p>hello world</p>\n\t</body>\n</html>"
 
 html = open('index.html','r+')
 html.write('%s%s%s%s%s' % (heading1, url,"'\">\n\t\t", nocaching,endtags))
--- staticpage.sh
+++ staticpage.sh
@@ -2,6 +2,7 @@
 lxterminal --command='./ngrokstart.sh'
 sleep 3
 curl 127.0.0.1:4040/inspect/http -o web.txt
+rm index.html
 python CNAME.py
 
 git add --all
